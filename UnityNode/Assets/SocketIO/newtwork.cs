716582fc40c8e6e612fea9d9671cb8ed94fc5cc8
George Codat Tudoran <office@solcreation.ro>
2016-11-22 10:35:30 +0200

    Linked Unity with the node.js server

---
+++ UnityNode/Assets/SocketIO/newtwork.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using SocketIO;
+
+public class newtwork : MonoBehaviour {
+
+    static SocketIOComponent socket;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    socket = GetComponent<SocketIOComponent>();
+        socket.On("open", onConnected);
+	}
+
+
+    void onConnected(SocketIOEvent e)
+    {
+        Debug.Log("connected from unity side");
+        socket.Emit("move");
+    }
+
+}
